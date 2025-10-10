68183dbd1519f5eb1d4c08a9029d99270dd3374e
Lars Reddering <lars@reddering.eu>
2025-10-10 18:47:54 +0200

    feat: Update footer to include Catcher24 logo and modify attribution text

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -48,12 +48,9 @@
       <div class="text-center mt-8 sm:mt-12 text-lg text-gray-500">
         <p>Laatst bijgewerkt: <span class="font-semibold">{{ lastUpdate }}</span></p>
         <div class="mt-4 text-2xl flex items-center justify-center gap-1 text-gray-600">
-          <span>2025 - Built with</span>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 text-blue-800">
-            <path
-              d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z" />
-          </svg>
-          <span>by <a href="https://github.com/LarsFixt" target="_blank" class="">LarsFixt</a></span>
+          <span><a href="https://github.com/LarsFixt" target="_blank" class="">LarsFixt</a> - Powered by</span>
+          <a href="https://catcher24.com"><img src="/img/logo_catcher24.svg" alt="Catcher logo" class="h-8 ml-1"
+              style="filter: invert(59%) sepia(65%) saturate(4065%) hue-rotate(352deg) brightness(101%) contrast(95%);" /></a>
         </div>
       </div>
     </div>
